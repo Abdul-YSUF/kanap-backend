6d81abadc89a78d262efc578da0eb21528a6b5e2
IALTAIR <yferoze@gmail.com>
2025-04-02 22:49:52 +0200

    modification

--- controllers/product.js
+++ controllers/product.js
@@ -1,92 +1,81 @@
-const uuid = require('uuid/v1');
-const Product = require('../models/Product');
+const { v4: uuidv4 } = require("uuid"); // Mise à jour de uuid
+const Product = require("../models/Product");
 
 exports.getAllProducts = (req, res, next) => {
-  Product.find().then(
-    (products) => {
+  Product.find()
+    .then((products) => {
       const mappedProducts = products.map((product) => {
-        product.imageUrl = req.protocol + '://' + req.get('host') + '/images/' + product.imageUrl;
+        if (product.imageUrl && !product.imageUrl.startsWith("http")) {
+          product.imageUrl = `${req.protocol}://${req.get("host")}/images/${
+            product.imageUrl
+          }`;
+        }
         return product;
       });
       res.status(200).json(mappedProducts);
-    }
-  ).catch(
-    () => {
-      res.status(500).send(new Error('Database error!'));
-    }
-  );
+    })
+    .catch(() => {
+      res.status(500).json({ error: "Database error!" });
+    });
 };
 
 exports.getOneProduct = (req, res, next) => {
-  Product.findById(req.params.id).then(
-    (product) => {
+  Product.findById(req.params.id)
+    .then((product) => {
       if (!product) {
-        return res.status(404).send(new Error('Product not found!'));
+        return res.status(404).json({ error: "Product not found!" });
       }
-      product.imageUrl = req.protocol + '://' + req.get('host') + '/images/' + product.imageUrl;
+      if (product.imageUrl && !product.imageUrl.startsWith("http")) {
+        product.imageUrl = `${req.protocol}://${req.get("host")}/images/${
+          product.imageUrl
+        }`;
+      }
       res.status(200).json(product);
-    }
-  ).catch(
-    () => {
-      res.status(500).send(new Error('Database error!'));
-    }
-  )
+    })
+    .catch(() => {
+      res.status(500).json({ error: "Database error!" });
+    });
 };
 
-/**
- *
- * Expects request to contain:
- * contact: {
- *   firstName: string,
- *   lastName: string,
- *   address: string,
- *   city: string,
- *   email: string
- * }
- * products: [string] <-- array of product _id
- *
- */
 exports.orderProducts = (req, res, next) => {
-  if (!req.body.contact ||
-      !req.body.contact.firstName ||
-      !req.body.contact.lastName ||
-      !req.body.contact.address ||
-      !req.body.contact.city ||
-      !req.body.contact.email ||
-      !req.body.products) {
-    return res.status(400).send(new Error('Bad request!'));
+  const { contact, products } = req.body;
+
+  if (
+    !contact ||
+    !contact.firstName ||
+    !contact.lastName ||
+    !contact.address ||
+    !contact.city ||
+    !contact.email ||
+    !products
+  ) {
+    return res.status(400).json({ error: "Bad request!" });
   }
-  let queries = [];
-  for (let productId of req.body.products) {
-    const queryPromise = new Promise((resolve, reject) => {
-      Product.findById(productId).then(
-        (product) => {
-          if (!product) {
-            reject('Product not found: ' + productId);
-          }
-          product.imageUrl = req.protocol + '://' + req.get('host') + '/images/' + product.imageUrl;
-          resolve(product);
-        }
-      ).catch(
-        () => {
-          reject('Database error!');
-        }
-      )
-    });
-    queries.push(queryPromise);
-  }
-  Promise.all(queries).then(
-    (products) => {
-      const orderId = uuid();
-      return res.status(201).json({
-        contact: req.body.contact,
-        products: products,
-        orderId: orderId
-      })
-    }
-  ).catch(
-    (error) => {
-      return res.status(500).json(new Error(error));
-    }
+
+  let queries = products.map((productId) =>
+    Product.findById(productId).then((product) => {
+      if (!product) {
+        throw new Error(`Product not found: ${productId}`);
+      }
+      if (product.imageUrl && !product.imageUrl.startsWith("http")) {
+        product.imageUrl = `${req.protocol}://${req.get("host")}/images/${
+          product.imageUrl
+        }`;
+      }
+      return product;
+    })
   );
+
+  Promise.all(queries)
+    .then((products) => {
+      const orderId = uuidv4();
+      res.status(201).json({
+        contact,
+        products,
+        orderId,
+      });
+    })
+    .catch((error) => {
+      res.status(500).json({ error: error.message });
+    });
 };
